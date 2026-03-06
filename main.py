9e54b5ce1c6ee86600280803f478f1cc1092493d
aniket00101 <dasaniket971@gmail.com>
2026-03-07 00:36:50 +0530

    mini commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def index ():
+    return "Hello World"
+
+@app.get('/dict')
+def dict():
+    return {
+        "data" : {
+            "name" : "Aniket"
+        }
+    }
