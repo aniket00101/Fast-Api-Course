1a51ef77919801ac9ec16ae64d412844a0c181d4
aniket00101 <dasaniket971@gmail.com>
2026-03-18 11:49:35 +0530

    added

--- doctor_appointment/main.py
+++ doctor_appointment/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 import json
 
 class Patient(BaseModel):
@@ -17,7 +17,7 @@ class Patient(BaseModel):
     @computed_field
     @property
     def bmi(self) -> float:
-        bmi = round(self.weight, (self.height**2),2)
+        bmi = round(self.weight/(self.height**2),2)
         return bmi 
     
     @computed_field
@@ -32,6 +32,13 @@ class Patient(BaseModel):
         else :
             return 'Obese'
 
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['male', 'female']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
 
 app = FastAPI()
 
@@ -81,3 +88,7 @@ def create_patient(patient: Patient):
     data[patient.id] = patient.model_dump(exclude=['id'])
     save_data(data)
     return JSONResponse(status_code=201, content={'message': 'patient created successfully'})
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, patient_update: PatientUpdate):
+    
